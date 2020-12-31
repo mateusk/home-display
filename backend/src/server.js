026e500a3ba6a7ea57bfb3a178f594d5e5e1fe8e
Mateus Knelsen <hello@mateus.works>
2020-12-31 20:39:48 +0100

    Update server.js to support routes and Media schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,36 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 const cors = require('cors')
+const PORT = process.env.PORT || 3001
+const dbURL = 'mongodb://localhost:27017/homedisplaydb'
 
+// Starting the app:
 const app = express()
+app.use(express.json())
+app.use(cors())
 
-var corsOptions = {
-  origin: 'http://localhost:8081',
-}
+// Conecting with the DB:
+mongoose
+  .connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to the database!')
+  })
+  .catch(err => {
+    console.log('Cannot connect to the database!', err)
+    process.exit()
+  })
 
-app.use(cors(corsOptions))
+// Requiring our models from our models folder:
+requireDir('./models')
 
-// Parse requests of content-type - application/json
-app.use(bodyParser.json())
+// Requiring our routes specified in /src/routes.js
+app.use('/api', require('./routes'))
 
-// Parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// Simple routing
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to home display application.' })
-})
-
-// Set port, listen for requests
-const PORT = process.env.PORT || 8080
+// Listening requests in port 3001:
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
