c73b5155f91c2db1da1b97d2a892610d98eddaa1
Mateus Knelsen <hello@mateus.works>
2020-12-31 20:38:53 +0100

    Add Media schema

---
+++ backend/src/models/Media.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate')
+
+const mediaSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  fileURL: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+// Adding the "mongoose-paginate" plugin, to allow
+// for pagination of requests:
+mediaSchema.plugin(mongoosePaginate)
+
+// Passing the above schema to a Mongoose model:
+mongoose.model('Media', mediaSchema)
