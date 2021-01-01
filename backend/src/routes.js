9c65e8bff1767fe87157a012c08ea2e2cf1660a2
Mateus Knelsen <hello@mateus.works>
2021-01-01 23:20:45 +0100

    Add random media request to the backend routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,7 +6,6 @@ const mediaController = require('./controllers/MediaController')
 
 routes.get('/media', mediaController.getAll)
 routes.get('/media/:id', mediaController.getSingle)
-routes.get('/randommedia', mediaController.getRandom)
 routes.post('/media', mediaController.store)
 routes.put('/media/:id', mediaController.update)
 routes.delete('/media/:id', mediaController.delete)
