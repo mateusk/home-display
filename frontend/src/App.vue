a14d4daa4a32d6bedaff5911fd244d4829a30547
Mateus Knelsen <sapientia_@hotmail.com>
2022-02-13 23:11:42 +0100

    Add basic app CSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,4 +4,15 @@
   <RouterView />
 </template>
 
-<style></style>
+<style>
+  body,
+  html,
+  #app {
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    font-size: 16px;
+    font-weight: 400;
+    background-color: rgb(0 0 0);
+  }
+</style>
