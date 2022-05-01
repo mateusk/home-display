0881606a96eda7f8e4c99459c5305b8d8701e1f0
Mateus Knelsen <sapientia_@hotmail.com>
2022-05-01 15:35:55 +0200

    Implement automatic timed media switch and transitions

--- frontend/src/components/MediaPlayer.vue
+++ frontend/src/components/MediaPlayer.vue
@@ -7,47 +7,126 @@
 
   const currentMediaType = ref(null)
   const currentMediaURL = ref(null)
+  const previousMediaType = ref(null)
+  const previousMediaURL = ref(null)
+  const isTranslating = ref(false)
 
-  const setCurrentMedia = () => {
-    const selectMediaArray = () =>
-      Math.ceil(Math.random(0, 2)) > 0.5 ? props.images : props.videos
+  function setCurrentMedia() {
     const mediaArray = selectMediaArray()
     currentMediaURL.value =
       mediaArray[Math.floor(Math.random() * mediaArray.length)]
-    const setCurrentMediaType = () => {
-      const fileExt = currentMediaURL.value.match(/(?:\.([^.]+))?$/)
-      if (
-        fileExt === 'jpg' ||
-        fileExt === 'jpeg' ||
-        fileExt === 'png' ||
-        fileExt === 'gif' ||
-        fileExt === 'bmp' ||
-        fileExt === 'webp'
-      ) {
-        return 'image'
-      }
-      return 'video'
-    }
+
+    console.log(currentMediaURL.value)
 
     currentMediaType.value = setCurrentMediaType()
   }
 
+  function selectMediaArray() {
+    return Math.ceil(Math.random(0, 2)) > 0.5 ? props.images : props.videos
+  }
+
+  function setCurrentMediaType() {
+    const fileExt = currentMediaURL.value.match(/(?:\.([^.]+))?$/)[1]
+    if (
+      fileExt === 'jpg' ||
+      fileExt === 'jpeg' ||
+      fileExt === 'png' ||
+      fileExt === 'gif' ||
+      fileExt === 'bmp' ||
+      fileExt === 'webp'
+    ) {
+      return 'image'
+    } else return 'video'
+  }
+
   onMounted(() => {
     setCurrentMedia()
+    setTimeout(() => {
+      isTranslating.value = true
+    }, 500)
+
+    setInterval(() => {
+      isTranslating.value = false
+      previousMediaURL.value = currentMediaURL.value
+      previousMediaType.value = currentMediaType.value
+      setCurrentMedia()
+      setTimeout(() => {
+        isTranslating.value = true
+      }, 500)
+    }, 20000)
   })
 </script>
 
 <template>
-  <img v-if="currentMediaType === 'image'" :src="currentMediaURL" alt="" />
-  <video v-else :src="currentMediaURL"><track kind="captions" /></video>
+  <div class="media-container-overlay">
+    <div v-if="previousMediaURL" class="previous-media-container">
+      <img
+        v-if="previousMediaType === 'image'"
+        :src="previousMediaURL"
+        alt=""
+      />
+      <video v-else>
+        <source :src="previousMediaURL" />
+        <track kind="captions" />
+      </video>
+    </div>
+    <div class="current-media-container">
+      <img
+        v-if="currentMediaType === 'image'"
+        :src="currentMediaURL"
+        alt=""
+        :class="{ translating: isTranslating, stopped: !isTranslating }"
+      />
+      <video v-else>
+        <source :src="currentMediaURL" />
+        <track kind="captions" />
+      </video>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-  img {
-    height: 100%;
+  .media-container-overlay {
+    display: grid;
+    width: 100%;
+    height: 100vh;
   }
 
+  .current-media-container,
+  .previous-media-container {
+    display: flex;
+    grid-area: 1 / 1;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .current-media-container {
+    z-index: 2;
+  }
+
+  .previous-media-container img,
   video {
-    height: 100%;
+    transform: translateX(-50%);
+  }
+
+  img,
+  video {
+    height: 100vh;
+    aspect-ratio: auto;
+    object-fit: cover;
+  }
+
+  .stopped {
+    opacity: 0;
+    transition: opacity 0s linear, transform 0s linear;
+    transform: translateX(0);
+  }
+
+  .translating {
+    opacity: 1;
+    transition: opacity 1s linear, transform 20s ease-in-out;
+    transform: translateX(-50%);
   }
 </style>
